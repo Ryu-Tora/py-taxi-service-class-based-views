ff0dec2d2e91ac10e26bd06ac3f98300b360c806
Adam Halka <galkaadam4@gmail.com>
2025-11-13 13:39:33 +0100

    Flake8 problems

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,11 +4,26 @@ from .views import index, ManufacturerListView, DriverListView, CarListView, Dri
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer_list"),
-    path("cars/", CarListView.as_view(), name="car_list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car"),
-    path("drivers/", DriverListView.as_view(), name="driver_list"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver"),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer_list"),
+    path(
+        "cars/",
+        CarListView.as_view(),
+        name="car_list"),
+    path(
+        "cars/<int:pk>/",
+        CarDetailView.as_view(),
+        name="car"),
+    path(
+        "drivers/",
+        DriverListView.as_view(),
+        name="driver_list"),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver"),
 ]
 
 app_name = "taxi"
