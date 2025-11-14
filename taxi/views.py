94378e65a6d52f338c2776012c3652a1422dfefa
Adam Halka <galkaadam4@gmail.com>
2025-11-14 09:03:14 +0100

    Code Verbesserung12

--- taxi/views.py
+++ taxi/views.py
@@ -50,6 +50,6 @@ class DriverDetailView(generic.DetailView):
     model = Driver
     context_object_name = "driver_detail"
     queryset = Driver.objects.prefetch_related(
-        "car_set__manufacturer"
+        "cars__manufacturer"
     ).order_by("username")
     template_name = "taxi/driver_detail.html"
